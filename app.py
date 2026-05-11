43d5a329aeeeb08a0228bb0bbd72695561b76a2c
Martins Valainis <valainis.martinss@gmail.com>
2026-05-11 19:03:06 +0300

    Pievienotas papildus HTML lapas un navigacija

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Sveika pasaule!</h1>"
+    return render_template("index.html")
+
+
+@app.route("/riders")
+def riders():
+    return render_template("riders.html")
+
+
+@app.route("/races")
+def races():
+    return render_template("races.html")
+
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
